7cf0ee43eb4ab0654d6b22f3678f2c45a5cea323
ChiragShamradeveloper <chiragsharmadeveloper@gmail.com>
2025-10-05 21:17:23 +0530

    submit

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+let app=express();
+app.use(express.json())   
+
+// Mogoose connection
+
+
+app.use('/register',userRouter)
+
+
+ app.listen(process.env.PORT, ()=>{
+    console.log('Server is running on port')
+})
+
